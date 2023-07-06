b041bb480e15ba86afa916c4d678bac4374a003f
dharmik-soni <137485971+dharmik-soni@users.noreply.github.com>
2023-07-06 15:44:11 +0530

    Add files via upload

---
+++ DAY 16/array.php
@@ -0,0 +1,12 @@
+<?php
+//using print_r
+    $array1 = array("key1"=>50,"key2"=>70,"key3"=>30);
+    print_r ($array1);
+    echo "<br>";
+    $array2 = array(50,20,40,30);
+    print_r ($array2);
+    echo "<br>";
+    $array_brackets = ["50","20","40","30"];
+    print_r ($array_brackets);
+
+?>
